b5fa5bc481c2bd3d7baf7d869daff54e5d914ce8
Dom-Dermann <dominikdermann@gmail.com>
2019-08-20 21:35:44 +0200

    year-by-year comparison of net profit margin and gross profit margin

--- bull_ratio_calculation.py
+++ bull_ratio_calculation.py
@@ -14,11 +14,13 @@ class company:
     # method to calculate all ratios
     def calculate_ratios(self):
         if self.historical_income_statements is not None:
+            # calculate individual margins as Pandas Series
             npm = calc_net_profit_margin(self.historical_income_statements)
-            print(npm)
+            gm = get_gross_margin(self.historical_income_statements)
 
-
-
+            # combine Pandas Series with ratios to one report in the form of a Pandas DataFrame
+            ratio_summary = pd.DataFrame({'net profit margin' : npm, 'gross profit margin' : gm})
+            print(ratio_summary)
 
 
 def calc_net_profit_margin(historical_is):
@@ -36,3 +38,17 @@ def calc_net_profit_margin(historical_is):
         hist_prof_margins[date_of_statement] = year_net_profit_margin
     
     return hist_prof_margins
+
+def get_gross_margin(historical_is):
+    hist_gross_margin = pd.Series()
+
+    for year_data in historical_is:
+        date_of_statement = year_data['date']
+        try:
+            year_gross_margin = float(year_data['Gross Margin'])
+        except:
+            pass
+        
+        hist_gross_margin[date_of_statement] = year_gross_margin
+    
+    return hist_gross_margin
