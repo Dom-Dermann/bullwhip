c2b32f302a8f72e5ac363a932118d0f98212be65
Dom-Dermann <dominikdermann@gmail.com>
2019-08-20 21:01:02 +0200

    command line argument for company symbol provided

--- bullwhip.py
+++ bullwhip.py
@@ -1,5 +1,13 @@
 import bull_api_calls
 import bull_ratio_calculation
+import argparse
 
-historical_is = bull_api_calls.get_stock_info("AAPL")
-bull_ratio_calculation.calc_income_ratios(historical_is)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Give the machine a company symbol to work with.")
+    parser.add_argument('company_symbol', nargs=1)
+    args = parser.parse_args()
+    c_symbol = args.company_symbol[0]
+
+    historical_is = bull_api_calls.get_stock_info(c_symbol)
+    if historical_is is not None:
+        bull_ratio_calculation.calc_income_ratios(historical_is)
